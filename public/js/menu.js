f38ac2353d78e05009b3e847d58831f9b311dd9d
Valentin MOREAU <vmoreau@datasolution.fr>
2023-04-11 18:15:27 +0200

    amélioration du menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,5 +1,12 @@
 let burgerBtn = document.querySelector("#burger_btn");
 let navLinksContainer = document.querySelector("#nav_links_container");
+let container = document.querySelector("#container");
 burgerBtn.addEventListener("click", () => {
   navLinksContainer.classList.toggle("hidden-custom");
+  burgerBtn.classList.toggle("is-active");
+  if(navLinksContainer.classList.contains("hidden-custom") === false) {
+    container.style.height = "100vh";
+  } else {
+    container.style.height = "auto";
+  }
 })
